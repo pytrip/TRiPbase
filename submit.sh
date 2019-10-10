e8396c7d2edd27bc1b3fd9d978f409c764a8a809
Niels Bassler <niels.bassler@gmail.com>
2019-10-10 16:35:34 +0200

    Add submission script.

---
+++ submit.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+wdir=`pwd`  # present working directory
+
+cd ${wdir}/wdir
+for shdir in $(find . -mindepth 3 -maxdepth 3 -type d )
+do
+	cd $shdir
+	echo $shdir
+	# echo sbatch --array 0-16 rsshield.sh
+	echo sbatch rsshield.sh
+	cd ${wdir}/wdir
+done
